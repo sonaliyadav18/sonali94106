c4f47ae97742d1d5ae7fd9581c045c5daa6fd48a
sonaliyadav18 <sonaliyadav1862005@gmail.com>
2025-12-19 09:20:54 +0530

    Day6

--- Day6/que3.py
+++ Day6/que3.py
@@ -1,38 +1,35 @@
 from flask import Flask
+import os
+
 server = Flask(__name__)
+
 @server.get('/')
-def homepage():
-    return "This is a home page"
+def home():
+    return "Sensor Server Running"
 
-@server.get('/welcome')
-def welcome():
-    return "<html><body><h1>Temperature and Light Intensity</h1></body></html>"
-
-@server.post('/temperature/<float:temp>')
+@server.route('/temperature/<float:temp>', methods=['GET', 'POST'])
 def post_temperature(temp):
-    print(f"temp = {temp}")
-
     with open("temperature.txt", "a") as t:
         t.write(str(temp) + "\n")
+    return f"Temperature {temp} stored"
 
-    return f"{temp} is received"
-
-@server.post('/light/<int:light>')
+@server.route('/light/<int:light>', methods=['GET', 'POST'])
 def post_light(light):
-    print(f"light = {light}")
-
     with open("light.txt", "a") as l:
         l.write(str(light) + "\n")
+    return f"Light {light} stored"
 
-    return f"{light} is received"
-
-@server.get('/temperature')
+@server.get('/get_temperature')
 def get_temperature():
+    if not os.path.exists("temperature.txt"):
+        return "No temperature data"
     with open("temperature.txt", "r") as t:
         return "<br>".join(t.readlines())
 
-@server.get('/light')
+@server.get('/get_light')
 def get_light():
+    if not os.path.exists("light.txt"):
+        return "No light data"
     with open("light.txt", "r") as l:
         return "<br>".join(l.readlines())
 
